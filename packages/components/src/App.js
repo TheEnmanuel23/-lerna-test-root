b6d6138db7110a5e71446b8c33ca8a50cda4e11b
TheEnmanuel23 <enmisac@gmail.com>
2022-07-03 00:07:48 -0600

    one change

--- packages/components/src/App.js
+++ packages/components/src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  console.log(123);
   return (
     <div className="App">
       <header className="App-header">

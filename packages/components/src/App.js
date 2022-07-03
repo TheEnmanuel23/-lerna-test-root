5ad41c001138036f79cc0bb3367fc789e2ec4dfe
TheEnmanuel23 <enmisac@gmail.com>
2022-07-03 00:20:20 -0600

    newchange

--- packages/components/src/App.js
+++ packages/components/src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  console.log("hi");
+  console.log("hello");
   return (
     <div className="App">
       <header className="App-header">

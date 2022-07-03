73454cd47a026ec9b6d3b320c9d1703f6f4f8f25
TheEnmanuel23 <enmisac@gmail.com>
2022-07-03 00:12:16 -0600

    second

--- packages/components/src/App.js
+++ packages/components/src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  console.log(123);
+  console.log("hi");
   return (
     <div className="App">
       <header className="App-header">

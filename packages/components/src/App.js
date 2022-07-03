02beacfa9bc98dccec8da35926d6cd8165a7188e
TheEnmanuel23 <enmisac@gmail.com>
2022-07-03 01:02:09 -0600

    new three

--- packages/components/src/App.js
+++ packages/components/src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  console.log("hola 19");
+  console.log("hola 20");
   return (
     <div className="App">
       <header className="App-header">
